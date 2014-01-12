8b3bfa02721e17e2afd3476add5822b527f53f3c
Neale Pickett <neale@woozle.org>
2014-01-12 11:07:42 -0700

    Standard wiring pin assignments

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define CLOCK_MHZ 20
+#define CLOCK_MHZ 16
 #define CLOCK_HZ (CLOCK_MHZ * 1000000)
 
 #define SCORE_DIGITS 2
@@ -9,11 +9,11 @@
 #define JAM_DIGITS 3
 
 // Set these to the PORTA pins you use
-#define SIN 0
-#define SCLK 1
-#define SLTCH 2
-#define NESCLK 3
-#define NESLTCH 4
-#define NESOUT 5
+#define SIN 3
+#define SCLK 4
+#define SLTCH 5
+#define NESCLK 0
+#define NESLTCH 1
+#define NESOUT 2
 
 #endif
